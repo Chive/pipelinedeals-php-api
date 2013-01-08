3b5256abc4c40695c3362a7fa155da4f4f053756
Kim Thoenen <kim@smuzey.ch>
2013-01-08 20:18:36 +0100

    Extended getData function
    Added conditions & page parameters

--- functions.php
+++ functions.php
@@ -16,14 +16,29 @@
 	/* API Adapter Function / Shorthand version
 	     Arguments
 		   $res		The wanted resource name, e.g.: "deals" or "admin/lead_statuses"
+		   $conditions	Conditions in key=>value array, e.g.: "array('[deal_created][from_date]'=>'2011-01-01','[deal_created][to_date]' => '2012-01-01')"
+		   $page	The wanted page as integer, e.g.: "2"
 		   $postdata	For POST requests, e.g.: "deal[name]=Kinda a big deal"
 		 Return value
 		   array	JSON decoded PHP array, ready to use in your application
 	*/
 		
-	function getData ($res,$postdata = false) {
+	function getData ($res, $conditions = null, $page = null, $postdata = false) {
 		global $api_key;
+		
+		// Base URL
 		$url = "https://api.pipelinedeals.com/api/v3/" . $res . ".json?api_key=" . $api_key;
+		
+		// Adding conditions
+		if (!empty($conditions)) {
+			foreach ($conditions as $key=>$value) {
+				$url .= "&conditions" . $key . "=" . $value;
+			}
+		}
+		
+		// Adding page
+		if (!empty($page)) { $url .= "&page=" . $page; }
+		
 		return getJSON(curl($url,$postdata));
 	}
 	
@@ -37,8 +52,15 @@
 	
 	function getDataURL ($url,$postdata = false) {
 		global $api_key;
-		$url2 = $url . "?api_key=" . $api_key;
-		return getJSON(curl($url2,$postdata));
+		
+		// Do we already have arguments in the URL query string
+		if (strstr($url, '?')) { $sign = '&'; }
+		else { $sign = '?';	}
+		
+		// Construct string
+		$url .= $sign . "api_key=" . $api_key;
+		
+		return getJSON(curl($url,$postdata));
 	}
 	
 	
