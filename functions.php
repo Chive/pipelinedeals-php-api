45c17a2c442a91edc97539eab1c41a6333301736
Kim Thoenen <kim@smuzey.ch>
2013-02-10 00:43:43 +0100

    Last update, this is deprecated.
    Use the new library instead!

--- functions.php
+++ functions.php
@@ -10,6 +10,9 @@
 	// Please set your API Key here
 	// You can get it from here: https://www.pipelinedeals.com/admin/api (API_KEY)
 	$api_key = '';
+
+	// Base URL (shouldn't be changed)
+	$base_url = "https://api.pipelinedeals.com/api/v3/";
 	
 	
 	/* API Adapter Function / Shorthand version
@@ -22,11 +25,11 @@
 		   array	JSON decoded PHP array, ready to use in your application
 	*/
 		
-	function getData ($res, $conditions = null, $page = null, $postdata = false) {
-		global $api_key;
+	function getData ($res, $conditions = null, $page = null) {
+		global $api_key; global $base_url;
 		
-		// Base URL
-		$url = "https://api.pipelinedeals.com/api/v3/" . $res . ".json?api_key=" . $api_key;
+		// Constructing URL
+		$url = $base_url . $res . ".json?api_key=" . $api_key;
 		
 		// Adding conditions
 		if (!empty($conditions)) {
@@ -38,7 +41,25 @@
 		// Adding page
 		if (!empty($page)) { $url .= "&page=" . $page; }
 		
-		return getJSON(curl($url,$postdata));
+		return getJSON(curl($url));
+	}
+	
+	function postData ($res, $postdata) {
+		global $api_key; global $base_url;
+
+		// Constructing URL
+		$url = $base_url . $res . ".json?api_key=" . $api_key;
+
+		return getJSON(curl($url,"post",$postdata));
+	}
+
+	function putData ($res, $putdata) {
+		global $api_key; global $base_url;
+
+		// Constructing URL
+		$url = $base_url . $res . ".json?api_key=" . $api_key;
+
+		return getJSON(curl($url,"put",$putdata));
 	}
 	
 	/* API Adapter Function / URL version
@@ -58,6 +79,14 @@
 		
 		// Construct string
 		$url .= $sign . "api_key=" . $api_key;
+
+		// POST data
+		$poststring = "";
+		if (!empty($postdata)) {
+			foreach ($postdata as $field=>$value) {
+				$poststring .= $field . "=" . $value . ";";
+			}
+		}
 		
 		return getJSON(curl($url,$postdata));
 	}
@@ -67,8 +96,8 @@
 	// Do not modify anything below! //
 	// ***************************** //
 		
-	// Basic cURL function	
-	function curl($url,$postdata = false) {
+	// Returns data from cURL request
+	function curl($url,$datatype = null, $data = null) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -78,10 +107,18 @@
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		
-		if ($postdata != false) {
-			curl_setopt($ch, CURLOPT_POST, 1); 
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
+		if (!empty($datatype)) {
+			if ($datatype == "post") {
+				curl_setopt($ch, CURLOPT_POST, 1); 
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+			}
+
+			else if ($datatype == "put") {
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); 
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+			}
 		}
+
 		
 		$res = curl_exec($ch);
 		curl_close($ch);
@@ -89,7 +126,7 @@
 		return $res;
 	}
 	
-	// Basic json_decode function
+	// Returns json_decoded resource
 	function getJSON ($res) {
 		return json_decode((string)$res,true);
 	}
